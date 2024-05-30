05d1d65f1a5b40aee226d960bbb370d18878662b
Hari Gokul <ceeeharigokul23@gmail.com>
2024-05-30 14:48:42 +0530

    JavaScript File

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function addnumbers(){
+
+    var num1= parseInt(document.getElementById("num1").value);
+    var num2= parseInt(document.getElementById("num2").value);
+
+    var sum = (num1) +(num2);
+
+    document.getElementById("result").textContent = `The Sum is ` + sum;
+}
